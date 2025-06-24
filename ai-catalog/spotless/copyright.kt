eb74abcc156395918987572d4b2a0a72fe2a408a
Thomas Ezan <tezan.enssat@gmail.com>
2025-06-24 16:17:21 -0700

    Apply spotless (#47)
    * Add spotless config
    
    * Apply spotless

--- ai-catalog/spotless/copyright.kt
+++ ai-catalog/spotless/copyright.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 The Android Open Source Project
+ * Copyright $YEAR The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,11 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.ai.samples.geminivideosummary.viewmodel
-
-sealed class OutputTextState {
-    data object Initial : OutputTextState()
-    data class Success(val text: String) : OutputTextState()
-    data class Error(val errorMessage: String) : OutputTextState()
-    data object Loading : OutputTextState()
-}
